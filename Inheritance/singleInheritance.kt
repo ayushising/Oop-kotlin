365ef7570c00c12ca5c48fbe8d00a0494886bf6f
Ayushi_Singh <66073819+ayushising@users.noreply.github.com>
2022-11-23 20:59:48 +0530

    Create singleInheritance.kt

---
+++ Inheritance/singleInheritance.kt
@@ -0,0 +1,18 @@
+open class baseClass{
+    val name = "Ayushi"
+    fun A(){
+        println("Base Class")
+    }
+}
+//derived class
+class derivedClass: baseClass() {
+    fun B() {
+        println(name)           //inherit name property
+        println("Derived class") 
+    }
+}
+fun main(args: Array<String>) {
+    val derived = derivedClass()
+    derived.A()         // inheriting the  base class function
+    derived.B()         // calling derived class function
+}
